9b5fb669b01871afc2067bbe43e6332e8921e508
Aditya Basu <aditya.basu1@yahoo.com>
2021-02-07 21:09:01 +0530

    Description and basic convergence code added

---
+++ convergence.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    double p = 0.5;
+
+    int n = 15,i;
+
+    cout<<p<<endl;
+    for(i=1; i<n; i++)
+    {
+        p = 1 - pow(p,2);
+        cout<<p<<endl;
+    }
+
+    return 0;
+}
+
