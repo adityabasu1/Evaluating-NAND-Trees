668be0bbd4275dcc9edfa159d6ec0ea61fd13ed2
Aditya Basu <aditya.basu1@yahoo.com>
2021-02-07 21:15:16 +0530

    Update convergence.cpp

--- convergence.cpp
+++ convergence.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-    double p = 0.5;
+    double p = 0.5; 
 
     int n = 15,i;
 
     cout<<p<<endl;
     for(i=1; i<n; i++)
     {
-        p = 1 - pow(p,2);
+        p = 1 - pow(p,2); // evaluates P1(n)
         cout<<p<<endl;
     }
 
